f18c5180640d97e0abe5248538efe6ef6b6e8795
sathishm <sathish8489@gmail.com>
2019-07-30 21:15:22 +0530

    First Commit
    Two Projects: one Random consoleApp and othe Wix Project.

---
+++ WixRandomApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WixRandomApp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("This is Test");
+            Console.Read();
+
+        }
+    }
+}
